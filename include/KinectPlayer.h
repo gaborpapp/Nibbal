5ea3f7416b0d06773d98fdd952b87c988aaa1c05
Gabor Papp <gabor.papp@gmail.com>
2012-07-27 08:13:25 +0200

    player skinning

--- include/KinectPlayer.h
+++ include/KinectPlayer.h
@@ -3,6 +3,7 @@
 #include <string>
 
 #include "cinder/Cinder.h"
+//#include "cinder/gl/Texture.h"
 #include "cinder/Quaternion.h"
 
 #include "PParams.h"
@@ -20,13 +21,22 @@ class KinectPlayer
 		void update();
 		void draw();
 
+		void showParams( bool show );
+
 	private:
 
 		mndl::ni::OpenNI mNI;
 		mndl::ni::UserTracker mNIUserTracker;
 
+		//ci::gl::Texture mDepthTexture;
+
 		mndl::assimp::AssimpLoader mPlayerAiMesh;
-		void setupBone( const std::string &name, const ci::Quatf &qrot );
+		void setupNode( const std::string &name, const ci::Quatf &qrot );
+		void transformNode( const std::string &nodeName, unsigned userId, XnSkeletonJoint skelJoint );
+
+		ci::params::PInterfaceGl mParams;
+		float mSmoothing;
+		float mMinOriConf;
 };
 
 } // namespace Nibbal
--- src/KinectPlayer.cpp
+++ src/KinectPlayer.cpp
@@ -7,11 +7,14 @@ using namespace ci;
 using namespace std;
 using namespace mndl;
 
+#define USE_KINECT 1
+
 namespace Nibbal {
 
 void KinectPlayer::setup( const fs::path &path )
 {
-	/*
+#if USE_KINECT
+	// setup kinect
 	if ( path.empty() )
 		mNI = ni::OpenNI( ni::OpenNI::Device() );
 	else
@@ -21,7 +24,13 @@ void KinectPlayer::setup( const fs::path &path )
 
 	mNI.setMirrored();
 	mNI.start();
-	*/
+#endif
+
+	mParams = params::PInterfaceGl( "Kinect", Vec2i( 300, 300 ) );
+	mParams.addPersistentSizeAndPosition();
+
+	mParams.addPersistentParam( "Smoothing", &mSmoothing, .7, "min=0 max=1 step=.05" );
+	mParams.addPersistentParam( "Min ori confidence", &mMinOriConf, .7, "min=0 max=1 step=.05" );
 
 	// load player
 	mPlayerAiMesh = assimp::AssimpLoader( app::getAssetPath( "models/player/player.dae" ) );
@@ -30,22 +39,22 @@ void KinectPlayer::setup( const fs::path &path )
 	// setup bind pose
 	Quatf q( Vec3f( 0, 1, 0 ), M_PI / 4 );
 	Quatf q2( Vec3f( 1, 0, 0 ), -M_PI / 2 );
-	setupBone( "root", q2 );
-	setupBone( "r_humerus", q * q2 );
-	setupBone( "r_ulna", q * q2 );
+	setupNode( "root", q2 );
+	setupNode( "r_humerus", q * q2 );
+	setupNode( "r_ulna", q * q2 );
 
 	q = Quatf( Vec3f( 0, 1, 0 ), -M_PI / 4 );
-	setupBone( "l_ulna", q * q2 );
-	setupBone( "l_humerus", q * q2 );
+	setupNode( "l_ulna", q * q2 );
+	setupNode( "l_humerus", q * q2 );
 
-	setupBone( "neck", q2 );
-	setupBone( "l_hip", q2 );
-	setupBone( "l_knee", q2 );
-	setupBone( "r_hip", q2 );
-	setupBone( "r_knee", q2 );
+	setupNode( "neck", q2 );
+	setupNode( "l_hip", q2 );
+	setupNode( "l_knee", q2 );
+	setupNode( "r_hip", q2 );
+	setupNode( "r_knee", q2 );
 }
 
-void KinectPlayer::setupBone( const string &name, const Quatf &qrot )
+void KinectPlayer::setupNode( const string &name, const Quatf &qrot )
 {
 	assimp::AssimpNodeRef nodeRef = mPlayerAiMesh.getAssimpNode( name );
 	if ( !nodeRef )
@@ -59,14 +68,68 @@ void KinectPlayer::setupBone( const string &name, const Quatf &qrot )
 	nodeRef->setInitialState();
 }
 
+void KinectPlayer::transformNode( const string &nodeName, unsigned userId, XnSkeletonJoint skelJoint )
+{
+#if USE_KINECT
+	float oriConf;
+	Matrix33f ori = mNIUserTracker.getJointOrientation( userId, skelJoint, &oriConf );
+
+	if ( oriConf >= mMinOriConf )
+	{
+		// change rotation coordinate directions
+		ori.m10 *= -1;
+		ori.m01 *= -1;
+		ori.m21 *= -1;
+		ori.m12 *= -1;
+		Quatf q( ori );
+		assimp::AssimpNodeRef nodeRef = mPlayerAiMesh.getAssimpNode( nodeName );
+		// apply skeleton pose relatively to the bone bind pose
+		nodeRef->setOrientation( nodeRef->getInitialOrientation() * q );
+	}
+#endif
+}
+
 void KinectPlayer::update()
 {
+#if USE_KINECT
+	/*
+	if ( mNI.checkNewDepthFrame() )
+		mDepthTexture = mNI.getDepthImage();
+	*/
+
+	mNIUserTracker.setSmoothing( mSmoothing );
+
+	vector< unsigned > users = mNIUserTracker.getUsers();
+	if ( !users.empty() )
+	{
+		unsigned userId = users[ 0 ];
+
+		transformNode( "root", userId, XN_SKEL_TORSO );
+		transformNode( "l_humerus", userId, XN_SKEL_LEFT_SHOULDER );
+		transformNode( "r_humerus", userId, XN_SKEL_RIGHT_SHOULDER );
+		transformNode( "l_ulna", userId, XN_SKEL_LEFT_ELBOW );
+		transformNode( "r_ulna", userId, XN_SKEL_RIGHT_ELBOW );
+	/*
+		transformNode( "l_hip", userId, XN_SKEL_LEFT_HIP );
+		transformNode( "r_hip", userId, XN_SKEL_RIGHT_HIP );
+		transformNode( "l_knee", userId, XN_SKEL_LEFT_KNEE );
+		transformNode( "r_knee", userId, XN_SKEL_RIGHT_KNEE );
+	*/
+		transformNode( "neck", userId, XN_SKEL_NECK );
+	}
+#endif
+
 	mPlayerAiMesh.update();
 }
 
 void KinectPlayer::draw()
 {
 	mPlayerAiMesh.draw();
+}
+
+void KinectPlayer::showParams( bool show )
+{
+	mParams.show( show );
 }
 
 } // namespace Nibbal
